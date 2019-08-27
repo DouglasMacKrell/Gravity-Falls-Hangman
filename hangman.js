027a7735017da15ca9cc2a4883b58c3feb9739e9
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-08-27 18:08:35 -0400

    removed console logs bug detectors

--- hangman.js
+++ hangman.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let youDoWantTo = document.querySelector("#yes")
     youDoWantTo.addEventListener("click", () => {
-        console.log("clicked p2")
         event.preventDefault()
         document.getElementById("page1").style.display = "none"
         document.getElementById("page2").style.display = "grid"
@@ -11,13 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let youDoNotWantTo = document.querySelector("#no")
     youDoNotWantTo.addEventListener("click", () => {
-        console.log("clicked no")
         window.location.href = "http://BigMacKrell.com"
     })
 
     let turnToPage3 = document.getElementById("press-to-talk1")
     turnToPage3.addEventListener("click", () => {
-        console.log("clicked p3")
         event.preventDefault()
         document.getElementById("page2").style.display = "none"
         document.getElementById("page3").style.display = "grid"
@@ -25,7 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let turnToPage4 = document.getElementById("press-to-talk2")
     turnToPage4.addEventListener("click", () => {
-        console.log("clicked p4")
         event.preventDefault()
         document.getElementById("page3").style.display = "none"
         document.getElementById("page4").style.display = "grid"
@@ -33,16 +29,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let turnToPage5 = document.getElementById("press-to-talk3")
     turnToPage5.addEventListener("click", () => {
-        console.log("clicked p5")
         event.preventDefault()
         document.getElementById("page4").style.display = "none"
         document.getElementById("page5").style.display = "grid"
     })
 
-    // let resetButton = document.querySelector("")
-
-
-    let arr = ["daddy", "bubble", "button", "stan", "grunkle", "dipper", "mabel", "soos", "mcgucket", "mcskermish", "greasys", "susan", "candy", "greta", "standord", "stanley", "dimensional", "caesar", "cipher", "blendin", "blandin", "journal", "cider", "summerween", "jackalope", "bigfoot", "alien", "gobblewonker", "mystery", "shack", "gravity", "triangle", "pacifica", "waddles"]
+    let arr = ["stan", "grunkle", "dipper", "mabel", "soos", "mcgucket", "mcskermish", "greasys", "susan", "candy", "greta", "standord", "stanley", "dimensional", "caesar", "cipher", "blendin", "blandin", "journal", "cider", "summerween", "jackalope", "bigfoot", "alien", "gobblewonker", "mystery", "shack", "gravity", "triangle", "pacifica", "waddles"]
 
     let threeLettersBack = "DEFGHIJKLMNOPQRSTUVWXYZABC"
 
@@ -50,11 +42,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let randomWord = arr[Math.floor(Math.random() * arr.length)]
 
-    console.log(randomWord)
+    console.log(`CHEAT CODE ACCEPTED: \nTHE ANSWER IS ${randomWord}`)
 
     let splitRandomWord = randomWord.split("")
-
-    console.log(splitRandomWord)
 
     let dashHolder = []
 
@@ -76,14 +66,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("dash-holder").innerHTML = dashHolder.join("");
     document.getElementById("correct-answer").innerHTML = `THE CORRECT ANSWER WAS: ${randomWord.toUpperCase()}`
 
-    console.log(dashHolder)
-    console.log(caesarHolder)
-    //console.log(dashHolder.join(""))
-
     let wrongAnswers = 0
     let win = false;
-
-    
     
     let form = document.querySelector("#form4")
     
@@ -96,25 +80,19 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             event.preventDefault()
             alert.innerText = ""
-            console.log(userInput)
             for(let i = 0; i < splitRandomWord.length; i++) {
                 if (userInput === splitRandomWord[i]) {
                     dashHolder[i] = `${userInput}`;
-                    console.log(dashHolder)
                     document.getElementById("dash-holder").innerHTML = (dashHolder.join("")).toUpperCase()
                 }
             }
             if(temp == dashHolder.join("")) {
                 wrongAnswers++
-                console.log(wrongAnswers)
-                console.log(typeof wrongAnswers)
             }
             if(wrongAnswers === 1) {
                 document.querySelector("#energy1").style.visibility = "visible"
-                console.log("gotcha")
             } else if (wrongAnswers === 2) {
                 document.getElementById("energy2").style.visibility = "visible"
-                console.log("gotcha2")
             } else if (wrongAnswers === 3) {
                 document.getElementById("energy3").style.visibility = "visible"
             } else if (wrongAnswers === 4) {
@@ -126,11 +104,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("final-dipper").src = "./Dipper Panicked CROP.png"
             }
          if (!dashHolder.includes("_ ")){
-            console.log("You win!!!")
             document.getElementById("page5").style.display = "none"
             document.getElementById("you-win").style.display = "flex"
          } else if (wrongAnswers === 7) {
-            console.log("You lose!")
             document.getElementById("page5").style.display = "none"
             document.getElementById("you-lose").style.display = "flex"
          }
